41b29dcd286ad0363ec0b74dcf47729542a321ee
jscogg <jscogg@gmail.com>
2013-10-07 20:47:12 -0500

    Basic set-up in place.

---
+++ index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Indent &bull; Home</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Styles -->
+	<link rel="stylesheet" href="css/styles.indev.css">
+	<!-- Web fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Arizonia' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-9 blue-border"></div>
+			<div class="col-lg-1 lblue"></div>
+			<div class="col-lg-1 offwht"></div>
+			<div class="col-lg-1 green"></div>
+		</div>
+		<div class="row">
+			<div class="col-lg-8">
+				<h1 class="title">Indent &para;<span class="subtitle"><em>Lorem ipsum dolor sit amet, consectetur adipisicing elit.<!-- The Online Starting Point for Mrs. Scoggin's English Classes --></em></span></h1>
+			</div>
+			<div class="col-lg-4">
+				<ul class="nav">
+					<li><a href="#" class="active">Home</a></li>
+					<li><a href="#">Classes</a>
+						<ul>
+							<li><a href="#">AP Language</a></li>
+							<li><a href="#">English 11</a></li>
+						</ul>
+					</li>
+					<li><a href="#">Links</a>
+						<ul>
+							<li><a href="http://edline.net">Edline</a></li>
+							<li><a href="http://edmodo.com">Edmodo</a></li>
+							<li><a href="http://odysseyware.com">Odysseyware</a></li>
+						</ul>
+					</li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-8">
+				<article>
+					<h2>Lorem ipsum dolor sit amet.</h2>
+					<h4><?php echo date(F d Y); ?></h4>
+					<p>Eligendi, reiciendis sequi quod itaque amet quo illo laboriosam aperiam accusantium soluta sit culpa quasi recusandae minus nihil. Beatae, unde, delectus, molestias, corrupti necessitatibus veniam tempore asperiores officia aliquid iusto illum suscipit tenetur similique atque fugit sint excepturi? Voluptate, dolore nam id.</p>
+					<p><img src="http://lorempixel.com/640/480/abstract/" alt="cats"></p>
+					<p>Sit id sint voluptate? Illum, id, deleniti, harum quasi perferendis delectus ab earum iste autem repellendus a.</p>
+				</article>
+				<article>
+					<h2>Lorem ipsum dolor sit amet.</h2>
+					<p>Eligendi, reiciendis sequi quod itaque amet quo illo laboriosam aperiam accusantium soluta sit culpa quasi recusandae minus nihil. Beatae, unde, delectus, molestias, corrupti necessitatibus veniam tempore asperiores officia aliquid iusto illum suscipit tenetur similique atque fugit sint excepturi? Voluptate, dolore nam id.</p>
+					<p>Sit id sint voluptate? Illum, id, deleniti, harum quasi perferendis delectus ab earum iste autem repellendus a.</p>
+				</article>
+			</div>
+			<div class="col-lg-4">
+				<aside class="sidebar">
+					<h2>Lorem ipsum dolor sit amet, consectetur adipisicing.</h2>
+					<p><img src="http://lorempixel.com/256/256/cats/" alt="cat"></p>
+					<p>Veritatis, porro, accusamus esse error expedita ratione harum amet aut sit veniam! Molestiae, earum, cum eos autem placeat numquam reiciendis nam repudiandae. Quis, suscipit, dolor eligendi quo quidem officiis debitis sint quaerat provident vero modi rerum omnis veritatis quod magnam! Laboriosam, doloribus?</p>
+				</aside>
+			</div>
+		</div>
+		<div class="row">
+				<div class="col-lg-12">
+					<footer>
+						<p><small>Copyright &copy; 2013 Indent. All rights reserved.</small></p>
+					</footer>
+				</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-9 blue-border"></div>
+			<div class="col-lg-1 lblue"></div>
+			<div class="col-lg-1 offwht"></div>
+			<div class="col-lg-1 green"></div>
+		</div>
+	</div>
+</body>
+</html>
